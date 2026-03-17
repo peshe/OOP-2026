f4c0fba8ca07f5803dda67133cc235ceb6ba6ac1
stefan <wreathbro@gmail.com>
2026-03-17 10:34:38 +0200

    personList.h - корекции

--- Семинари/ИС4/mar17/personlist/personList.h
+++ Семинари/ИС4/mar17/personlist/personList.h
@@ -4,19 +4,20 @@
 
 #include "person.h"
 
-#include <cstding>
+#include <cstddef>
 
 struct PersonList
 {
     size_t size;
     Person * data;
-};z
+};
 
 
 bool personListInit(PersonList & list);
 bool personListDestroy(PersonList & list);
 bool personListAdd(PersonList & list, Person const & p);
 bool personListDelete(PersonList & list, size_t idx);
+
 
 bool personListPrint(PersonList const & list);
 
